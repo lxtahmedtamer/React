7a5cd1d582bd023c63b7155b620f8c2f95500cbd
lxtahmedtamer <Ahmed.Tamer@lxt.ai>
2024-09-03 11:28:22 +0300

    Create Links.js

---
+++ Links.js
@@ -0,0 +1,14 @@
+// Links.js
+import React from 'react';
+import './Links.css'; // Optional: create a separate CSS file for Links if needed
+
+const Links = () => {
+  return (
+    <div className="links">
+      <a href="https://www.linkedin.com/in/ahmed-tamer-b8699b282/" target="_blank" rel="noopener noreferrer">Connect with me on LinkedIn</a> |
+      <a href="https://github.com/lxtahmedtamer" target="_blank" rel="noopener noreferrer">Check out my GitHub</a>
+    </div>
+  );
+};
+
+export default Links;
